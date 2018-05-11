9ef8f7a9f26ac74e3f8719ac82f859ed6bbbb811
Julius Regalado <jreg03@outlook.com>
2018-05-11 19:54:00 -0400

    trying to deploy

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ if(process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.port || 5000;
