279799883597eab468da2ba69c68ba0b6751a4d0
Julius Regalado <jreg03@outlook.com>
2018-05-11 19:07:29 -0400

    prepared to deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport')
+const path = require('path');
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -27,6 +28,15 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+//SERVER STATIC ASSETS IF IN PRODUCTION
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
